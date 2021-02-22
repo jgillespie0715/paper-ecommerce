facb1576e7038c5a5045e8b8d2886b2e0c88c5c1
Justin Gillespie <jgillespie0715@gmail.com>
2021-02-22 14:25:31 -0500

    Feat: add homepage & scss styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 function App() {
-	return <div className='App'>Paper-ecommerce-client</div>;
+	return (
+		<div>
+			<HomePage />
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/homepage.component.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+function HomePage() {
+	return (
+		<div className='homepage'>
+			<div className='directory-menu'>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>SHARE CREATIONS</h1>
+						<span className='subtitle'>CREATE NOW</span>
+					</div>
+				</div>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>JOURNALS</h1>
+						<span className='subtitle'>SHOP NOW</span>
+					</div>
+				</div>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>PENS</h1>
+						<span className='subtitle'>SHOP NOW</span>
+					</div>
+				</div>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>PENCILS</h1>
+						<span className='subtitle'>SHOP NOW</span>
+					</div>
+				</div>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>MARKERS</h1>
+						<span className='subtitle'>SHOP NOW</span>
+					</div>
+				</div>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>CALENDARS</h1>
+						<span className='subtitle'>SHOP NOW</span>
+					</div>
+				</div>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>CARDS</h1>
+						<span className='subtitle'>SHOP NOW</span>
+					</div>
+				</div>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>ENVELOPES</h1>
+						<span className='subtitle'>SHOP NOW</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default HomePage;

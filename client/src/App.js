8d41e95e91bf8bcfcda3c0bb307ce2f2d52fff45
Justin Gillespie <jgillespie0715@gmail.com>
2021-02-24 23:15:35 -0500

    Feat: user view products
    *user can navigate from homepage to
    shop-all menu page and select from
    product categories
    *products for that category have
    individual page
    *user alternative view all
    product listings through
    shop link in navigation header
    
    *reasoning is to separate main
    functionalities of app
    (blog and e-commerce platform)
    *main shop-all link more
    visual appeal, header shop
    link utilitarian
    *all data in context folders
    until server api functional
    *images in remote store
    *additional cleanup for file
    name consistency
    *update styles
    *add logo icon
    *Test: manual test non-
    breaking

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopAllPage from './pages/shopAll/shop-all-page.component';
+import HomePage from './pages/home-page/homepage.component';
+import ShopAllPage from './pages/shop-all/shop-all-page.component';
+import ShopPage from './pages/shop-page/shop-page.component';
+import Header from './components/header/header.component';
 
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path='/' component={HomePage} />
 				<Route exact path='/shop-all' component={ShopAllPage} />
+				<Route path='/shop' component={ShopPage} />
 			</Switch>
 		</div>
 	);

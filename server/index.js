8e1fc8eb7b374acc0be8a643301baf6819c76f0b
Justin Gillespie <jgillespie0715@gmail.com>
2021-03-02 13:35:14 -0500

    Chore: separate  functionality into files
    separate models, routes, and services into
    files for readability

--- server/index.js
+++ server/index.js
@@ -1,44 +1,75 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const connectDB = require('./models');
+const passport = require('passport');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 const path = require('path');
-const keys = require('../config/keys');
 const cors = require('cors');
-const router = require('./routes');
+const dotenv = require('dotenv');
+const refresh = require('passport-oauth2-refresh');
+const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
+const { OAuth2Strategy: GoogleStrategy } = require('passport-google-oauth');
+//const googleStrategyConfig = require('./services/passport');
 
 module.exports = () => {
 	const app = express();
-
-	// testing db connection
-	(async function connect(req, res, next) {
-		mongoose.Promise = global.Promise;
-		try {
-			await mongoose.connect(keys.mongoURI, {
-				useMongoClient: true,
-				serverSelectionTimeoutMS: 5000,
-				family: 4,
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			});
-		} catch (error) {
-			console.error('error connecting to db');
-			console.log('error: ', error);
-		}
-	})();
-
+	// server config and db connect
+	dotenv.config({ path: 'config/.env' });
+	connectDB();
 	app.use(cors());
-
 	app.use(bodyParser.json());
-
+	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(
 		cookieSession({
 			maxAge: 30 * 24 * 60 * 60 * 1000,
-			keys: [keys.cookieKey],
+			keys: [process.env.COOKIEKEY],
 		})
 	);
 
-	app.use('/api', router);
+	// Passport OAuth session
+	app.use(passport.initialize());
+	app.use(passport.session());
+
+	passport.serializeUser((user, done) => {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser((id, done) => {
+		User.findById(id).then((user) => {
+			done(null, user);
+		});
+	});
+
+	// Google OAUth login
+	const googleStrategyConfig = new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_SECRET,
+			callbackURL: 'http://localhost:8080/auth/google/callback',
+		},
+		async (accessToken, refreshToken, profile, done) => {
+			try {
+				const existingUser = await User.findOne({ googleId: profile.id });
+				if (existingUser) {
+					return done(null, existingUser);
+				}
+				const user = await new User({
+					googleId: profile.id,
+					displayName: profile.displayName,
+				}).save();
+				done(null, user);
+			} catch (err) {
+				done(err, null);
+			}
+		}
+	);
+
+	passport.use('google', googleStrategyConfig);
+	//refresh.use('google', googleStrategyConfig);
+
+	app.use('/api', blogRoutes);
+	app.use('/auth', authRoutes);
 
 	if (['production'].includes(process.env.NODE_ENV)) {
 		app.use(express.static(path.join(__dirname, 'client/build')));
@@ -48,17 +79,14 @@ module.exports = () => {
 		});
 	}
 
-	app.use((err, req, res, next) => {
-		const defaultErr = {
-			log: 'Express error handler caught unknown middleware error',
-			status: 400,
-			message: { err: 'An error occurred' },
-		};
+	app.use((error, req, res, next) => {
+		if (!error.statusCode) error.statusCode = 500;
 
-		const errorObj = Object.assign(defaultErr, err);
-		console.log(errorObj.message);
-		console.log(err);
-		return res.status(errorObj.status).json(errorObj.message);
+		if (error.statusCode === 301) {
+			return res.status(301).redirect('/not-found');
+		}
+
+		return res.status(error.statusCode).json({ error: error.toString() });
 	});
 
 	return app;

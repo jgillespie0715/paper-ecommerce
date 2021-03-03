cb55da3affd9e6939ba8af56c5287fc575a7cbb6
Justin Gillespie <jgillespie0715@gmail.com>
2021-03-02 19:57:01 -0500

    Revert "Merge branch 'development' into server"
    This reverts commit 9b5998008bdeb0c1c4520df1b8675afc020122dc, reversing
    changes made to 8e1fc8eb7b374acc0be8a643301baf6819c76f0b.

--- client/src/contexts/collection/collection.context.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-import COLLECTION_DATA from './collection.data';
-
-const CollectionContext = createContext(COLLECTION_DATA);
-
-export default CollectionContext;
--- client/src/contexts/shop-all-directory/shop-all-directory.context.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-import SHOPALL_DIRECTORY_DATA from './shop-all-directory.data';
-
-const ShopAllDirectoryContext = createContext(SHOPALL_DIRECTORY_DATA);
-
-export default ShopAllDirectoryContext;

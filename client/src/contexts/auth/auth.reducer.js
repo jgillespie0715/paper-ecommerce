60107b72a9fa27ff7f4cc1fff0c98505610228a6
Justin Gillespie <jgillespie0715@gmail.com>
2021-03-04 12:43:48 -0500

    Chore: implement auth context, types, and state
    improve state management over
    prop drilling
    *implementation not complete,
    need to import into components
    and finalize server routes

--- client/src/contexts/auth/auth.reducer.js
+++ client/src/contexts/auth/auth.reducer.js
@@ -1,9 +1,29 @@
 import { FETCH_USER } from '../types';
+import AuthActionTypes from './auth.types';
 
 function AuthReducer(state, action) {
 	switch (action.type) {
 		case FETCH_USER:
 			return action.payload || false;
+		case AuthActionTypes.SIGN_IN_SUCCESS:
+			return {
+				...state,
+				currentUser: action.payload,
+				error: null,
+			};
+		case AuthActionTypes.SIGN_OUT_SUCCESS:
+			return {
+				...state,
+				currentUser: null,
+				error: null,
+			};
+		case AuthActionTypes.SIGN_IN_FAILURE:
+		case AuthActionTypes.SIGN_OUT_FAILURE:
+		case AuthActionTypes.SIGN_UP_FAILURE:
+			return {
+				...state,
+				error: action.payload,
+			};
 		default:
 			return state;
 	}
